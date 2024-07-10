65eaabd2a9eae99f4b8e9f5381e6daa07a7ff2e1
Carlos Ramos <carlosmarioramos34@gmail.com>
2024-07-09 21:58:42 -0500

    Add router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Container from './components/Container'
-import Card from './components/Card'
-import cards from './data/cards.json'
+import Container from '../components/Container'
+import Card from '../components/Card'
+import cards from '../data/cards.json'
 
-export default function App() {
+export default function Home() {
 	return (
-		<section>
+		<section className='py-10'>
 			<Container className={'grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}>
 				{
 					cards.map(card => {

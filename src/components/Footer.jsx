18fecf28b42d88470175bf256714368b2cba20fc
Carlos Ramos <carlosmarioramos34@gmail.com>
2024-07-20 13:49:51 -0500

    Modify layout styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className='bg-neutral-900 py-10 px-10 text-white text-center'>
+		<footer className='bg-neutral-900 py-10 px-10 text-white text-center mt-auto'>
 			{new Date().getFullYear()}
 		</footer>
 	)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,9 @@ import Logo from './Logo'
 import Nav from './Nav'
 
 export default function Header() {
-	const height = 'h-16'
 	return (
-		<header className={`bg-white shadow w-full fixed top-0 left-0 z-10 ${height}`}>
-			<Container className={`flex justify-between items-center ${height}`}>
+		<header className='bg-white fixed top-0 left-0 z-10 shadow w-full h-16'>
+			<Container className={`flex justify-between items-center h-full`}>
 				<Logo />
 				<Nav />
 			</Container>

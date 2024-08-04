cbe51aa9d54ab5dbb002563cdcfcc56f081488a6
Carlos Ramos <carlosmarioramos34@gmail.com>
2024-08-03 20:57:26 -0500

    Apply Button component in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import Img from './Img'
+import Button from './Button'
 
 export default function Card({ img, title, desciption, className, ...res }) {
 	return (
@@ -9,7 +10,10 @@ export default function Card({ img, title, desciption, className, ...res }) {
 			<div className='p-4'>
 				<h4 className='text-lg font-semibold hover:underline hover:cursor-pointer'>{title}</h4>
 				<p className='text-sm text-[#62646A] text-ellipsis line-clamp-3 leading-5'>{desciption}</p>
-				<button className='w-full py-2 px-4 border mt-2 hover:bg-gray-200'>Click</button>
+				<Button
+					text={'Click'}
+					className='w-full py-2 px-4 border mt-2 hover:bg-gray-200'
+				/>
 			</div>
 		</article>
 	)

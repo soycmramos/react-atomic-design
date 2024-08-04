5d56e0652d1545f6b76fd5ce7dce4a44da8cb233
Carlos Ramos <carlosmarioramos34@gmail.com>
2024-08-03 20:55:05 -0500

    Adjust layout and container component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import Header from './Header'
 import Footer from './Footer'
+import Container from './Container'
 
 export default function Layout({ children }) {
 	return (
 		<>
 			<Header />
 			<main>
-				{children}
+				<Container>
+					{children}
+				</Container>
 			</main>
 			<Footer />
 		</>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,21 @@
 import { useEffect } from 'react'
-import Container from '../components/Container'
 import Card from '../components/Card'
 import cards from '../data/cards.json'
 
 export default function Home() {
 	useEffect(() => { document.title = 'Home' }, [])
 	return (
-		<section className='py-10'>
-			<Container className={'grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}>
-				{
-					cards.map(card => {
-						return (
-							<Card
-								key={card.id}
-								img={card.img}
-								title={card.title}
-								desciption={card.description}
-							/>
-						)
-					})
-				}
-			</Container>
+		<section className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+			{
+				cards.map(card => (
+					<Card
+						key={card.id}
+						img={card.img}
+						title={card.title}
+						desciption={card.description}
+					/>
+				))
+			}
 		</section>
 	)
 }
